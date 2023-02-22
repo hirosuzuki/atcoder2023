6dcd745cd8752360d0e27666acfd63fbd656e842
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2023-02-22 15:15:23 +0000

    ABC 290 B

---
+++ abc290/abc290_b.py
@@ -0,0 +1,19 @@
+# https://atcoder.jp/contests/abc290/tasks/abc290_a
+
+N, K = [int(x) for x in input().split()]
+S = input()
+
+
+def solve(N: int, K: int, S: str):
+    rs = list(S)
+    j = K
+    for i in range(len(rs)):
+        if rs[i] == "o":
+            if j <= 0:
+                rs[i] = "x"
+            else:
+                j -= 1
+    print(*rs, sep="")
+
+
+solve(N, K, S)
