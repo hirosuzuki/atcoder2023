52ba543f07e352c46a45c3798e794f54d9a2dfca
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2023-02-25 16:41:03 +0000

    ABC 269 A

---
+++ abc269/abc269_a.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc269/tasks/abc269_a
+
+A, B, C, D = [int(x) for x in input().split()]
+
+
+def solve(A: int, B: int, C: int, D: int):
+    print((A + B) * (C - D))
+    print("Takahashi")
+
+
+solve(A, B, C, D)
