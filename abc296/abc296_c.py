aaff076b26547e85ea8e6aace2f1eefaf12d00af
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2023-04-05 15:28:36 +0000

    ABC 296 C (refactor)

--- abc296/abc296_c.py
+++ abc296/abc296_c.py
@@ -8,16 +8,11 @@ A = [int(x) for x in input().split()]
 
 
 def solve(N: int, X: int, A: List[int]):
-    def check(t: int, xs: List[int]) -> bool:
-        r = bisect(xs, t)
-        if r > 0 and xs[r - 1] == t:
-            return True
-        return False
 
-    xs = sorted(set(A))
+    sa = set(A)
 
-    for a in xs:
-        if check(a + X, xs):
+    for a in sa:
+        if a + X in sa:
             print("Yes")
             return
 
